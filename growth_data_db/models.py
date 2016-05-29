497dcf02217a957f08a32200da7c4b1fe430bf19
Peter Tonner <peter.tonner@gmail.com>
2016-05-29 13:31:53 -0400

    remove chemicals

--- growth_data_db/models.py
+++ growth_data_db/models.py
@@ -33,7 +33,6 @@ class Well(Base):
 	plate_id = Column(Integer, ForeignKey('plates.id'))
 	plate = relationship("Plate", back_populates="wells")
 	plate_number = Column(Integer)
-	quantities = relationship("ChemicalQuantity",back_populates="well")
 	design_values = relationship("DesignValue",back_populates="well")
 
 	def __repr__(self):
@@ -73,41 +72,6 @@ class DesignValue(Base):
 
 	def __repr__(self):
 		return "%s %s" % (self.value,self.design.name)
-
-class Chemical(Base):
-	__tablename__ = "chemicals"
-	id = Column(Integer, primary_key=True)
-	name = Column(String,unique=True)
-	abbreviation = Column(String,unique=True)
-	quantities = relationship("ChemicalQuantity",back_populates="chemical")
-
-	def __repr__(self):
-		return "%s (%s)" % (self.name, self.abbreviation)
-
-class ChemicalQuantity(Base):
-	__tablename__ = "chemical_quantities"
-	id = Column(Integer, primary_key=True)
-	type = Column(Enum("concentration","mass by volume","percent mass","percent volume"))
-	chemical_id = Column(Integer, ForeignKey("chemicals.id"))
-	chemical = relationship("Chemical",back_populates="quantities")
-	well_id = Column(Integer, ForeignKey("wells.id"))
-	well = relationship("Well",back_populates="quantities")
-	value = Column(Float)
-
-	# each well should only have one quantity of any single chemical
-	__table_args__ = (UniqueConstraint('well_id','chemical_id', name='_well_chemical_uc'),
-                     )
-
-	def parse(self,s):
-		value,_,chemical = s.split(" ")
-		return value == self.value and (chemical==self.chemical.name or chemical==self.chemical.abbreviation)
-
-	def __repr__(self):
-		if self.type == "concentration":
-			if self.chemical.abbreviation and self.chemical.abbreviation != "":
-				return "%.2lf mM %s (%s)" % (self.value, self.chemical.abbreviation,self.well)
-			else:
-				return "%.2lf mM %s (%s)" % (self.value, self.chemical.name,self.well)
 
 class Strain(Base):
 	__tablename__ = "strains"
