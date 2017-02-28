d4a07e08428ab2c008d22c29332acbd6ef8d2881
Peter <peter.tonner@duke.edu>
2017-02-28 13:58:21 -0500

    remove references to api

--- popmachine/core/operation/plate_create.py
+++ popmachine/core/operation/plate_create.py
@@ -1,8 +1,67 @@
 from operation import PlateOperation
 from ...models import Plate, Well
-from ... import api
+# from ... import api
 import logging
 import pandas as pd
+
+def create_plate_data_table(plate, core):
+	"""create a data_table for the provided plate, doing nothing if it already exists."""
+
+	if not plate.data_table is None:
+		return core.metadata.tables[plate.data_table]
+
+	well_numbers = [w.plate_number for w in plate.wells]
+
+	cols = [Column('id', Integer, primary_key=True), Column('time', Float)] + \
+		[Column(str(wn), Float) for wn in well_numbers]
+
+	table = Table("_plate_data_%d"%plate.id,core.metadata,*cols)
+	core.metadata.create_all(core.engine)
+
+	plate.data_table = table.name
+	core.session.add(plate)
+	core.session.commit()
+
+	return table
+
+# def copy_plate_dataframe_to_table(data,table,data_columns,column_names):
+#
+# 	conn = engine.connect()
+# 	ins = table.insert()
+#
+# 	# add each row to the table
+# 	for i in range(data.shape[0]):
+# 		newrow = dict([('time',data.iloc[i,0])] + [(cn,data.iloc[i,j]) for cn,j in zip(column_names,data_columns)])
+# 		conn.execute(ins,**newrow)
+
+def add_experimental_design(core,design_name,design_value,*args,**kwargs):
+	# check if design exists, create if needed
+	design = core.session.query(models.Design).filter(models.Design.name==design_name).one_or_none()
+	if not design:
+		if not 'design_type' in kwargs:
+			logging.warning("design %s does not exist, and no design_type is specified to create a new one. using str as default"%design_name)
+			design_type='str'
+		else:
+			design_type = kwargs['design_type']
+
+		design = models.Design(name=design_name,type=design_type)
+		core.session.add(design)
+
+	experimentalDesign = core.session.query(models.ExperimentalDesign).filter(models.ExperimentalDesign.design==design,models.ExperimentalDesign.value==design_value).one_or_none()
+
+	if experimentalDesign is None:
+		logging.warning("ExperimentalDesign %s=%s does not exist, creating one"%(design_name, design_value))
+		experimentalDesign = models.ExperimentalDesign(design=design, value=design_value)
+
+	# for each well
+	# check if design value exists, create if needed
+	for well in args:
+		if isinstance(well,models.Well):
+			# dv = models.ExperimentalDesign(design=design,well=well,value=design_value)
+			well.experimentalDesigns.append(experimentalDesign)
+			# core.session.add(dv)
+
+	core.session.commit()
 
 class PlateCreate(PlateOperation):
 
@@ -34,7 +93,7 @@ class PlateCreate(PlateOperation):
         self.core.session.add_all(wells)
         self.core.session.commit()
 
-        table = api.upload.create_plate_data_table(self.plate,self.core)
+        table = create_plate_data_table(self.plate,self.core)
         self.core.session.commit()
 
         # copy in data
@@ -51,4 +110,4 @@ class PlateCreate(PlateOperation):
             for u in self.meta[c].unique():
                 select = self.meta[c] == u
                 temp = [w for w,s in zip(wells, select.tolist()) if s]
-                api.add_experimental_design(self.core,c,u,*temp)
+                add_experimental_design(self.core,c,u,*temp)
