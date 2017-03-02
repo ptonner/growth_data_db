d53035926004695b5782461490c31e47f17afdab
Peter Tonner <peter.tonner@gmail.com>
2017-03-02 09:35:50 -0500

    test updates

--- tests/test_plate_create.py
+++ tests/test_plate_create.py
@@ -5,8 +5,22 @@ import pandas as pd
 
 machine = popmachine.Machine('.test.db')
 
+# utility for making character string with limited range
+charstring = st.characters(min_codepoint=1, max_codepoint=100, blacklist_categories=('Cc', 'Cs')),min_size=1)
+
 @st.composite
-def buildData(draw):
+def buildMeta(draw, numDesigns=None, maxFactors=10, maxTreatments=100):
+
+    if numDesigns is None:
+        numDesigns = draw(st.lists(st.integers(min_value=1, max_size=maxTreatments), min_size=1, max_size=maxFactors))
+
+    designs = [draw(st.lists(st.text(charstring,min_size=1), min_size=d, max_size=d)) for d in numDesigns]
+
+    meta = [list(x) for x in itertools.product(*designs)]
+    return meta
+
+@st.composite
+def buildDataset(draw):
     # p = draw(st.integers(min_value=1, max_value=100))
     # l = draw(st.integers(min_value=1, max_value=100))
 
@@ -26,13 +40,13 @@ def buildData(draw):
 
     return data, meta
 
-@given(buildData())
+@given(buildDataset())
 def test_build_data_shape(data):
     data, meta = data
 
     assert data.shape[1] -1 == meta.shape[0]
 
-@given(buildData())
+@given(buildDataset())
 def test_creating_plate(data):
     data, meta = data
     num = len(list(machine.list(popmachine.models.Plate)))
