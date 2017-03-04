97568e97744d010b2ab97f8649e8b315e00258cd
Peter Tonner <peter.tonner@gmail.com>
2017-03-04 00:21:58 -0500

    update search test

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,5 +1,5 @@
 import popmachine, utils
-from hypothesis import given, settings
+from hypothesis import given, settings, find
 import hypothesis.strategies as st
 import unittest
 import numpy as np
@@ -18,10 +18,13 @@ class TestSearch(unittest.TestCase):
 
         assert search == dataset, search
 
-    @given(platename, platename.filter(lambda x: not x in machine.plates(names=True)), fullfactorialDataset)
+        machine.deletePlate(name)
+
+    @given(platename, platename, fullfactorialDataset)
     @settings(max_examples=10)
     def test_search_ignores_garbage_plate_names(self,name, other,dataset):
-        utils.machine.createPlate(name,data=dataset.data,experimentalDesign=dataset.meta)
+
+        plate = utils.machine.createPlate(name,data=dataset.data,experimentalDesign=dataset.meta)
 
         search = machine.search(plates=[name, other], include=dataset.meta.columns)
 
@@ -29,17 +32,62 @@ class TestSearch(unittest.TestCase):
 
         assert search == dataset, search
 
-    @given(utils.platename.filter(lambda x: not x in machine.plates(names=True)), utils.fullfactorialDataset)
+        machine.deletePlate(name)
+
+    # @given(utils.platename.filter(lambda x: not x in machine.plates(names=True)),\
+    @given(utils.platename,\
+            utils.fullfactorialDataset)
     @settings(max_examples=10)
     def test_search_individual_samples(self, name, ds):
 
-        utils.machine.createPlate(name,data=ds.data,experimentalDesign=ds.meta)
+        plate = utils.machine.createPlate(name,data=ds.data,experimentalDesign=ds.meta)
+
+        assert not plate is None
 
         for i, r in ds.meta.iterrows():
             search = machine.search(plates=[name], **r)
             del search.meta['plate']
 
             assert ds.data.iloc[:,i].equals(search.data[0])
+
+        machine.deletePlate(name)
+
+    @given(st.lists(\
+                    # utils.platename.filter(lambda x: not x in machine.plates(names=True)),\
+                    utils.platename,\
+                    min_size=2,max_size=2, unique=True),\
+            utils.fullfactorialDataset)
+    @settings(max_examples=10)
+    # @settings(suppress_health_check=True)
+    def test_search_matching_design_single_plate(self, names, ds):
+
+        p1 = utils.machine.createPlate(names[0],data=ds.data,experimentalDesign=ds.meta)
+        p2 = utils.machine.createPlate(names[1],data=ds.data,experimentalDesign=ds.meta)
+
+        for c in ds.meta.columns:
+            search = utils.machine.search(plates=names[0], c=ds.meta[c].unique())
+            assert not names[1] in search.meta['plate']
+
+        machine.deletePlate(names[0])
+        machine.deletePlate(names[1])
+
+    # @given(utils.platename.filter(lambda x: not x in machine.plates(names=True)),\
+    @given(utils.platename,\
+            utils.fullfactorialDataset,\
+            st.lists(utils.charstring.filter(lambda x: not x in machine.designs(names=True)), min_size=1))
+    # @settings(suppress_health_check=True)
+    @settings(max_examples=10)
+    def test_search_individual_samples_with_garbage_include(self, name, ds, other):
+
+        utils.machine.createPlate(name,data=ds.data,experimentalDesign=ds.meta)
+
+        for i, r in ds.meta.iterrows():
+            search = machine.search(plates=[name], include=other, **r)
+            del search.meta['plate']
+
+            assert ds.data.iloc[:,i].equals(search.data[0])
+
+        machine.deletePlate(name)
 
     # @given(utils.fullfactorialDataset, utils.fullfactorialDataset, utils.fullfactorialDataset)
     # @settings(max_examples=5)
