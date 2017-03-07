8626f158e95218359c41ddea05476dddf47345c5
Peter <peter.tonner@duke.edu>
2017-03-07 11:45:21 -0500

    update main

--- popmachine/__main__.py
+++ popmachine/__main__.py
@@ -1,21 +1,21 @@
 import argparse, logging, api
-from core import Core
+from popmachine import Machine
 from operation import PlateCreate, PlateDelete, DesignList, DesignSetType, SearchOperation
 import pandas as pd
 from models import Plate, Well, Design, ExperimentalDesign, Base
 from sqlalchemy import Table, Column, Integer, String, Interval, MetaData, ForeignKey, Float, or_, and_
 
-core = None
+machine = None
 
 def Init(args, warn=False):
 
-    project = core.session.query(Project).filter(Project.name==args.project).one_or_none()
+    project = machine.session.query(Project).filter(Project.name==args.project).one_or_none()
 
     if project is None:
         if warn:
             logging.warning("creating new project %s"%args.project)
         project = Project(name=args.project,plates=[])
-        core.session.add(project)
+        machine.session.add(project)
 
     return project
 
@@ -23,39 +23,12 @@ def ProjectList(args):
 
     # project = Init(args,warn=True)
 
-    for plate in core.session.query(Plate).all():
+    for plate in machine.session.query(Plate).all():
         print plate
-
-# class PlateCommand(object):
-#
-#     def __init__(self,args):
-#         self.args = args
-#
-#         self.project = core.session.query(Project).filter(Project.name==args.project).one_or_none()
-#         if self.project is None:
-#             logging.warning("creating new project %s"%self.args.project)
-#             self.project = Project(name=args.project,plates=[])
-#             core.session.add(self.project)
-#
-#         self.plate = core.session.query(Plate).filter(Plate.name==args.plate, Plate.project==self.project).one_or_none()
-#
-#     def run(self):
-#         self._run()
-#
-#     def _run(self):
-#         raise NotImplemented()
-#
-# class PlateDelete(PlateCommand):
-#
-#     def _run(self):
-#         if not self.plate is None:
-#             if not self.plate.data_table is None:
-#                 core.metadata.tables[self.plate.data_table].drop(core.engine)
-#             core.session.delete(self.plate)
 
 def main():
 
-    global core
+    global machine
 
     #############
     # Parsing
@@ -71,18 +44,7 @@ def main():
     search = subparsers.add_parser("search", help='search database')
     search.add_argument("--designs", help='designs to search', nargs="*", default=[])
     search.add_argument("--values", help='values to search', nargs="*", default=[])
-    search.set_defaults(func=lambda x: SearchOperation.fromArgs(core, x).run())
-
-    # Project
-    # project = subparsers.add_parser('project',help="project commands")
-    # project.add_argument('project', type=str,
-    #                     help='project in the database')
-    # projectSubparsers = project.add_subparsers(help="project sub-commands")
-
-    #   init
-
-    # init = projectSubparsers.add_parser('init', help='initialize a project')
-    # init.set_defaults(func = lambda x: ProjectOperation.fromArgs(core, x, createIfMissing=True))
+    search.set_defaults(func=lambda x: SearchOperation.fromArgs(machine, x).run())
 
     #   list
     _list = subparsers.add_parser('list', help='list plates in the database')
@@ -104,7 +66,7 @@ def main():
         return kw
 
     plateCreate = plateSubparsers.add_parser('create', help='create new plate')
-    plateCreate.set_defaults(func=lambda x: PlateCreate.fromArgs(core, x, createIfMissing=True, **designKwargs(x.designs)).run())
+    plateCreate.set_defaults(func=lambda x: PlateCreate.fromArgs(machine, x, createIfMissing=True, **designKwargs(x.designs)).run())
     plateCreate.add_argument("data", help='data file')
     plateCreate.add_argument("experimentalDesign", help='experimental design file')
     plateCreate.add_argument("--timeColumn",type=int,default=0)
@@ -113,7 +75,7 @@ def main():
     #       delete
 
     plateDelete = plateSubparsers.add_parser('delete', help='remove a plate')
-    plateDelete.set_defaults(func=lambda x: PlateDelete.fromArgs(core, x).run())
+    plateDelete.set_defaults(func=lambda x: PlateDelete.fromArgs(machine, x).run())
 
     # Design
 
@@ -124,14 +86,14 @@ def main():
     #   list
 
     designList = designSubparsers.add_parser("list", help='list designs')
-    designList.set_defaults(func=lambda x: DesignList.fromArgs(core, x).run())
+    designList.set_defaults(func=lambda x: DesignList.fromArgs(machine, x).run())
 
     #   setType
 
     designSetType = designSubparsers.add_parser("setType", help = 'set design type')
     designSetType.add_argument("design")
     designSetType.add_argument("type")
-    designSetType.set_defaults(func=lambda x: DesignSetType.fromArgs(core,x).run())
+    designSetType.set_defaults(func=lambda x: DesignSetType.fromArgs(machine,x).run())
 
     #############
 
@@ -141,7 +103,7 @@ def main():
     args,extras = parser.parse_known_args()
 
 
-    core = Core(args.database)
+    machine = Machine(args.database)
 
     # print args
 
@@ -152,7 +114,7 @@ def main():
 
     args.func(args)
 
-    core.session.commit()
+    machine.session.commit()
 
 if __name__=="__main__":
     main()
