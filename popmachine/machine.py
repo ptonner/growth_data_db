645b3ebaff7904fd2c2a50093708540afa8fd9da
Peter Tonner <peter.tonner@gmail.com>
2017-04-17 21:52:46 -0400

    add warning

--- popmachine/machine.py
+++ popmachine/machine.py
@@ -66,38 +66,75 @@ class Machine(Core):
 
         meta = data = None
 
-        for w in wells:
-            cols = [Column('time', Float), Column(str(w.plate_number), Float)]
-            table = self.metadata.tables[w.plate.data_table]
+        plates = self.session.query(Plate).join(Well)
+        plates = plates.filter(Well.id.in_([w.id for w in wells]))
+
+        for p in plates:
+            subwells = wells.filter(Well.plate==p)
+            cols = [Column('time', Float)]+[Column(str(w.plate_number), Float) for w in subwells]
+
+            table = self.metadata.tables[p.data_table]
             s = select(cols,from_obj=table)
             res = self.engine.execute(s)
 
-            newdata = pd.DataFrame(list(res), columns = ['time', "%d_%d"%(w.plate.id,w.plate_number)])
+            newdata = pd.DataFrame(list(res), columns = ['time'] + ["%d_%d"%(p.id,w.plate_number) for w in subwells])
             if data is None:
                 data = newdata
             else:
-                data = pd.merge(data,newdata,on='time', how='outer')
+                data = pd.merge(data,newdata,on='time')
 
-            newmeta = [w.plate.name, w.plate_number]
-            for d in designs:
-                ed = self.session.query(ExperimentalDesign).filter(\
-                                             ExperimentalDesign.design==d,\
-                                             ExperimentalDesign.wells.contains(w)).one_or_none()
+            newmeta = []
+            for w in subwells:
+                newmeta.append([p.name, w.plate_number])
+                for c in metacols:
+                    design = self.session.query(Design).filter(Design.name==c).one()
+                    ed = self.session.query(ExperimentalDesign).filter(\
+                                                 ExperimentalDesign.design==design,\
+                                                 ExperimentalDesign.wells.contains(w)).one()
+                    newmeta[-1].append(ed.get_value())
 
-                if not ed is None:
-                    newmeta.append(ed.get_value())
-                else:
-                    newmeta.append(None)
-            newmeta = pd.DataFrame([newmeta], columns = ['plate', 'number']+[d.name for d in designs])
-
+            newmeta = pd.DataFrame(newmeta, columns = ['plate', 'number']+metacols)
             if meta is None:
                 meta = newmeta
             else:
-                meta = pd.concat((meta, newmeta),ignore_index=True)
+                meta = pd.concat((meta, newmeta))
+
+        # for w in wells:
+        #     cols = [Column('time', Float), Column(str(w.plate_number), Float)]
+        #     table = self.metadata.tables[w.plate.data_table]
+        #     s = select(cols,from_obj=table)
+        #     res = self.engine.execute(s)
+        #
+        #     newdata = pd.DataFrame(list(res), columns = ['time', "%d_%d"%(w.plate.id,w.plate_number)])
+        #     if data is None:
+        #         data = newdata
+        #     else:
+        #         data = pd.merge(data,newdata,on='time', how='outer')
+        #
+        #     newmeta = [w.plate.name, w.plate_number]
+        #     for d in designs:
+        #         ed = self.session.query(ExperimentalDesign).filter(\
+        #                                      ExperimentalDesign.design==d,\
+        #                                      ExperimentalDesign.wells.contains(w)).one_or_none()
+        #
+        #         if not ed is None:
+        #             newmeta.append(ed.get_value())
+        #         else:
+        #             newmeta.append(None)
+        #     newmeta = pd.DataFrame([newmeta], columns = ['plate', 'number']+[d.name for d in designs])
+        #
+        #     if meta is None:
+        #         meta = newmeta
+        #     else:
+        #         meta = pd.concat((meta, newmeta),ignore_index=True)
 
         data.index = data.time
         del data['time']
         data = data.astype(float)
+
+        for i in include:
+            if not i in meta.columns:
+                logging.warning('no design %s found, ignored' % i)
 
         return DataSet(data, meta)
 
