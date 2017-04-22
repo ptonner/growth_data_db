0869e72abd3ddf57fc1d1d05c08fe115702575ef
Peter Tonner <peter.tonner@gmail.com>
2017-04-22 15:55:49 -0400

    remove comments

--- popmachine/machine.py
+++ popmachine/machine.py
@@ -2,8 +2,7 @@ from core import Core
 from models import Plate, Design, Well, ExperimentalDesign, well_experimental_design
 from dataset import DataSet
 from sqlalchemy.sql import select, false
-from sqlalchemy.orm import aliased
-from sqlalchemy import Column, Float, or_, and_
+from sqlalchemy import Column, Float
 import pandas as pd
 from plate import create, delete
 import search.query
@@ -99,35 +98,6 @@ class Machine(Core):
             else:
                 meta = pd.concat((meta, newmeta))
 
-        # for w in wells:
-        #     cols = [Column('time', Float), Column(str(w.plate_number), Float)]
-        #     table = self.metadata.tables[w.plate.data_table]
-        #     s = select(cols,from_obj=table)
-        #     res = self.engine.execute(s)
-        #
-        #     newdata = pd.DataFrame(list(res), columns = ['time', "%d_%d"%(w.plate.id,w.plate_number)])
-        #     if data is None:
-        #         data = newdata
-        #     else:
-        #         data = pd.merge(data,newdata,on='time', how='outer')
-        #
-        #     newmeta = [w.plate.name, w.plate_number]
-        #     for d in designs:
-        #         ed = self.session.query(ExperimentalDesign).filter(\
-        #                                      ExperimentalDesign.design==d,\
-        #                                      ExperimentalDesign.wells.contains(w)).one_or_none()
-        #
-        #         if not ed is None:
-        #             newmeta.append(ed.get_value())
-        #         else:
-        #             newmeta.append(None)
-        #     newmeta = pd.DataFrame([newmeta], columns = ['plate', 'number']+[d.name for d in designs])
-        #
-        #     if meta is None:
-        #         meta = newmeta
-        #     else:
-        #         meta = pd.concat((meta, newmeta),ignore_index=True)
-
         data.index = data.time
         del data['time']
         data = data.astype(float)
@@ -157,156 +127,8 @@ class Machine(Core):
 
         wells = search.query.query(wells, **kwargs)
 
-        # for k, v in kwargs.iteritems():
-        #     design = self.session.query(Design).filter(Design.name==k).one_or_none()
-        #     if design is None:
-        #         continue
-        #
-        #     valueAlias = aliased(ExperimentalDesign)
-        #
-        #     wells = wells.join(valueAlias, Well.experimentalDesigns)
-        #
-        #     if isinstance(v, list):
-        #         # convert non-string values
-        #         v = [str(z) for z in v]
-        #         wells = wells.filter(and_(valueAlias.design_id==design.id, valueAlias.value.in_(v)))
-        #     else:
-        #         # convert non-string values
-        #         v = str(v)
-        #         wells = wells.filter(and_(valueAlias.design_id==design.id, valueAlias.value==v))
-
         return wells
-
-        # for d in designs:
-        #     ed = self.session.query(ExperimentalDesign).filter(\
-        #                                     ExperimentalDesign.design==d)
-        #     v = kwargs[d.name]
-        #     if isinstance(v, list):
-        #         # convert non-string values
-        #         v = [str(z) for z in v]
-        #         ed = ed.filter(ExperimentalDesign.value.in_(v))
-        #     else:
-        #         # convert non-string values
-        #         v = str(v)
-        #         ed = ed.filter(ExperimentalDesign.value==v)
-        #
-        #     wells = wells.filter(ExperimentalDesign.id.in_([e.id for e in ed]))
-        #
-        # return wells
 
     def search(self, plates=[], numbers=[], include=[], *args, **kwargs):
         q = self.filter(plates, numbers, **kwargs)
         return self.get(q, include+kwargs.keys())
-
-    # def search2(self, plates=[], numbers=[],include=[], *args, **kwargs):
-    #     """search the database for wells matching the provided kwargs
-    #
-    #     arguments:
-    #     For each key, value pair filter wells with matching experimental designs. If the value of the pair is a list, any possible value in the list is accepted."""
-    #
-    #     wells = self.session.query(Well)
-    #
-    #     # if plates provided, filter on those
-    #     if isinstance(plates, list) and len(plates)>0:
-    #         wells = wells.join(Plate)
-    #         wells = wells.filter(Plate.name.in_(plates))
-    #     elif isinstance(plates, str) or isinstance(plates, unicode):
-    #         wells = wells.join(Plate)
-    #         wells = wells.filter(Plate.name==plates)
-    #         plates = [plates]
-    #
-    #     # if numbers are provided, filter those
-    #     if isinstance(numbers, list) and len(numbers)>0:
-    #         wells = wells.filter(Well.plate_number.in_(numbers))
-    #
-    #     metacols = []
-    #
-    #     for k,v in kwargs.iteritems():
-    #         design = self.session.query(Design).filter(Design.name==k).one_or_none()
-    #
-    #         if design is None:
-    #             continue
-    #         metacols.append(k)
-    #
-    #         experimentalDesigns = self.session.query(ExperimentalDesign).filter(\
-    #                                         ExperimentalDesign.design==design)
-    #
-    #         if isinstance(v, list):
-    #             # convert non-string values
-    #             v = [str(z) for z in v]
-    #
-    #             experimentalDesigns = experimentalDesigns.filter(\
-    #                                             ExperimentalDesign.value.in_(v))
-    #         else:
-    #             # convert non-string values
-    #             v = str(v)
-    #
-    #             experimentalDesigns = experimentalDesigns.filter(\
-    #                                             ExperimentalDesign.value==v)
-    #
-    #
-    #         filterwells = []
-    #         for ed in experimentalDesigns.all():
-    #             filterwells.extend([w.id for w in ed.wells if not w in filterwells and not w is None])
-    #
-    #         # wells = wells.filter(Well.id.in_(filterwells))
-    #         if len(filterwells)>0:
-    #             wells = wells.filter(Well.id.in_(filterwells))
-    #         else:
-    #             wells = wells.filter(false())
-    #
-    #     if wells.count()==0:
-    #         return None
-    #
-    #     # don't duplicate columns we're already using
-    #     include = filter(lambda x : not x in metacols, include)
-    #
-    #     for i in include:
-    #         design = self.session.query(Design).filter(Design.name==i).one_or_none()
-    #
-    #         if design is None or design.name in metacols:
-    #             continue
-    #         metacols.append(i)
-    #
-    #     # plates = self.session.query(Plate).filter(Plate.id.in_(set([w.plate.id for w in wells])))
-    #     plates = self.session.query(Plate).join(Well)
-    #     plates = plates.filter(Well.id.in_([w.id for w in wells]))
-    #
-    #     data = None
-    #     meta = None
-    #
-    #     for p in plates:
-    #         subwells = wells.filter(Well.plate==p)
-    #         cols = [Column('time', Float)]+[Column(str(w.plate_number), Float) for w in subwells]
-    #
-    #         table = self.metadata.tables[p.data_table]
-    #         s = select(cols,from_obj=table)
-    #         res = self.engine.execute(s)
-    #
-    #         newdata = pd.DataFrame(list(res), columns = ['time'] + ["%d_%d"%(p.id,w.plate_number) for w in subwells])
-    #         if data is None:
-    #             data = newdata
-    #         else:
-    #             data = pd.merge(data,newdata,on='time', how='outer')
-    #
-    #         newmeta = []
-    #         for w in subwells:
-    #             newmeta.append([p.name, w.plate_number])
-    #             for c in metacols:
-    #                 design = self.session.query(Design).filter(Design.name==c).one()
-    #                 ed = self.session.query(ExperimentalDesign).filter(\
-    #                                              ExperimentalDesign.design==design,\
-    #                                              ExperimentalDesign.wells.contains(w)).one()
-    #                 newmeta[-1].append(ed.get_value())
-    #
-    #         newmeta = pd.DataFrame(newmeta, columns = ['plate', 'number']+metacols)
-    #         if meta is None:
-    #             meta = newmeta
-    #         else:
-    #             meta = pd.concat((meta, newmeta))
-    #
-    #     data.index = data.time
-    #     del data['time']
-    #     data = data.astype(float)
-    #
-    #     return DataSet(data, meta)
