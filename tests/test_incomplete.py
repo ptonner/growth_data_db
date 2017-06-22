8cb43f5956b0b61dd80825a9fd5d1a091bd1e7ff
Peter <peter.tonner@duke.edu>
2017-06-22 17:20:42 -0400

    change health check

--- tests/test_incomplete.py
+++ tests/test_incomplete.py
@@ -1,4 +1,4 @@
-from hypothesis import given
+from hypothesis import given, settings, HealthCheck
 import hypothesis.strategies as st
 from dataset.incomplete import dataset, sharedDesignSpace
 
@@ -12,6 +12,7 @@ def test_incomplete_dataset_comes_from_designspace(dsp, ds):
     assert_dataset_in_designspace(ds, dsp)
 
 @given(sharedDesignSpace, st.lists(dataset(sharedDesignSpace), min_size=1,max_size=5))
+@settings(suppress_health_check=[HealthCheck.data_too_large])
 def test_incomplete_compedia_comes_from_designspace(dsp, datasets):
     for ds in datasets:
         assert_dataset_in_designspace(ds, dsp)
